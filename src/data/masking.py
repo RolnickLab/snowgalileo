714164c40e8416f87f65cbb428ba9ed3ce2f1e76
Gabriel Tseng <gabriel.tseng@mail.mcgill.ca>
2024-03-01 12:09:39 +0800

    Masking by blocks

--- src/data/masking.py
+++ src/data/masking.py
@@ -3,6 +3,9 @@ from enum import Enum
 from typing import Tuple
 
 import numpy as np
+from einops import repeat
+
+from .config import CROMA_INPUT_SIZE, VIT_PATCH_SIZE
 
 
 class MaskingStrategy(Enum):
@@ -12,8 +15,8 @@ class MaskingStrategy(Enum):
     PRESTO_TO_PRESTO = 3
 
 
-MaskOutput = namedtuple(
-    "MaskOutput", ["dynamic_input", "static_input", "dynamic_mask", "static_mask"]
+MaskedOutput = namedtuple(
+    "MaskedOutput", ["dynamic_input", "static_input", "dynamic_mask", "static_mask"]
 )
 
 
@@ -40,3 +43,44 @@ def subset_image(
     return dynamic_input[start_h : start_h + size, start_w : start_w + size], static_input[
         start_h : start_h + size, start_w : start_w + size
     ]
+
+
+def mask_presto_to_croma(
+    dynamic_input: np.ndarray, static_input: np.ndarray, mask_ratio: float
+) -> MaskedOutput:
+    """
+    Given a >CROMA_INPUT_SIZE>CROMA_INPUT_SIZE input:
+    1. Crops to CROMA_INPUT_SIZExCROMA_INPUT_SIZE
+    2. Masks out blocks of VIT_PATCH_SIZExVIT_PATCH_SIZE.
+        e.g. if CROMA_INPUT_SIZE=4 and VIT_PATCH_SIZE=2 and mask_ratio=0.25,
+        then a mask might look like
+        [0 0 1 1]
+        [0 0 1 1]
+        [0 0 0 0]
+        [0 0 0 0]
+        Where the top VIT_PATCH (2x2 pixels) is masked out.
+    3. This mask is then repeated along the channel and time dimensions to match
+       the dynamic and static input shapes
+    """
+    assert mask_ratio % (1 / ((CROMA_INPUT_SIZE / VIT_PATCH_SIZE) ** 2)) == 0
+    num_patches_per_dim = int(CROMA_INPUT_SIZE / VIT_PATCH_SIZE)
+    dynamic_input, static_input = subset_image(dynamic_input, static_input, CROMA_INPUT_SIZE)
+    # for the Presto to CROMA case, we will just remove blocks.
+    # To begin with, we compute a flat "mask" of patches
+    num_masked_patches = int((num_patches_per_dim**2) * mask_ratio)
+    flat_spatial_mask = np.concatenate(
+        (np.ones(num_masked_patches), np.zeros(num_patches_per_dim**2 - num_masked_patches))
+    )
+    np.random.shuffle(flat_spatial_mask)
+    spatial_mask = np.reshape(flat_spatial_mask, (num_patches_per_dim, num_patches_per_dim))
+    # then we go from CROMA token space (16x16 pixels) back to pixel space
+    pixel_spatial_mask = np.repeat(
+        np.repeat(spatial_mask, repeats=VIT_PATCH_SIZE, axis=0), repeats=VIT_PATCH_SIZE, axis=1
+    )
+    # expand the temporal and band dims so they match the dynamic and static input shapes
+    dynamic_mask = repeat(
+        pixel_spatial_mask, "h w -> h w t c", t=dynamic_input.shape[2], c=dynamic_input.shape[3]
+    )
+    static_mask = repeat(pixel_spatial_mask, "h w -> h w c", c=static_input.shape[2])
+
+    return MaskedOutput(dynamic_input, static_input, dynamic_mask, static_mask)
