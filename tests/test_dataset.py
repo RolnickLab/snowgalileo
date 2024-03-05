38e62d4ca65cba2b45fda5743eddc0f630ce3e63
Gabriel Tseng <gabriel.tseng@mail.mcgill.ca>
2024-03-05 23:15:00 +0800

    Fix NaNs in the dataset

--- tests/test_dataset.py
+++ tests/test_dataset.py
@@ -7,13 +7,15 @@ from src.data.dataset import DYNAMIC_BANDS, STATIC_BANDS, Dataset
 
 TEST_FILE = (
     Path(__file__).parents[1]
-    / "data/tifs/tifs_min_lat=19.2005_min_lon=-155.6227_max_lat=19.2132_max_lon=-155.6094_dates=2022-01-01_2023-12-31.tiff"
+    / "data/tifs/tifs_min_lat=19.2005_min_lon=-155.6227_max_lat=19.2132_max_lon=-155.6094_dates=2022-01-01_2023-12-31.tif"
 )
 
 
 class TestDataset(unittest.TestCase):
     def test_tif_to_array(self):
         dynamic_data, static_data, months = Dataset.tif_to_array(TEST_FILE)
+        self.assertFalse(np.isnan(dynamic_data).any())
+        self.assertFalse(np.isnan(static_data).any())
         self.assertEqual(static_data.shape[0], dynamic_data.shape[0])
         self.assertEqual(static_data.shape[1], dynamic_data.shape[1])
         self.assertEqual(len(DYNAMIC_BANDS), dynamic_data.shape[-1])
@@ -22,11 +24,3 @@ class TestDataset(unittest.TestCase):
         # makes sure that file hasn't changed.
         assert "dates=2022-01-01_2023-12-31" in TEST_FILE.name
         self.assertEqual(months[0], 0)
-
-        # one way to check this is correct is to see if all the DYNAMIC_WORLD bands
-        # sum to 1
-        dynamic_world_bands = [x for x in DYNAMIC_BANDS if x.startswith("DW_")]
-        dynamic_world_only = dynamic_data[:, :, :, -len(dynamic_world_bands) - 1 : -1].sum(axis=-1)
-        self.assertTrue(
-            np.allclose(dynamic_world_only[~np.isnan(dynamic_world_only)], 1, atol=0.01)
-        )
