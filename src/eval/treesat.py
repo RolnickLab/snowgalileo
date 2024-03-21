d727fb7204801b6b00091e292ef6c7718643906e
Gabriel Tseng <gabriel.tseng@mail.mcgill.ca>
2024-03-21 12:49:02 +0800

    TreeSat dataset

---
+++ src/eval/treesat.py
@@ -0,0 +1,188 @@
+import json
+import random
+from copy import deepcopy
+from pathlib import Path
+from typing import Tuple
+
+import numpy as np
+import rioxarray
+import torch
+from einops import repeat
+from torch.utils.data import Dataset
+
+from ..data.dataset import (
+    DYNAMIC_BANDS,
+    DYNAMIC_BANDS_GROUPS_IDX,
+    S1_BANDS,
+    STATIC_BAND_GROUPS_IDX,
+    STATIC_BANDS,
+)
+from ..data.earthengine.s2 import S2_BANDS
+from ..masked_datasets import MaskedOutput
+from ..utils import data_dir
+
+treesat_dir = "treesat"
+s1_files_dir = "s1/60m"
+s2_files_dir = "s2/60m"
+labels_file = "TreeSatBA_v9_60m_multi_labels.json"
+
+# https://zenodo.org/record/6780578
+# Band order is B02, B03, B04, B08, B05, B06, B07, B8A, B11, B12, B01, and B09.
+# Spatial resolution is 10 m.
+S2_BAND_ORDERING = ["B2", "B3", "B4", "B8", "B5", "B6", "B7", "B8A", "B11", "B12", "B1", "B9"]
+# Band order is VV, VH, and VV/VH ratio. Spatial resolution is 10 m.
+S1_BAND_ORDERING = ["VV", "VH", "VV/VH"]
+
+
+class TreeSatDataset(Dataset):
+    labels_to_int = {
+        "Abies": 0,
+        "Acer": 1,
+        "Alnus": 2,
+        "Betula": 3,
+        "Cleared": 4,
+        "Fagus": 5,
+        "Fraxinus": 6,
+        "Larix": 7,
+        "Picea": 8,
+        "Pinus": 9,
+        "Populus": 10,
+        "Prunus": 11,
+        "Pseudotsuga": 12,
+        "Quercus": 13,
+        "Tilia": 14,
+    }
+
+    num_timesteps = 1
+    # this is not the true start month!
+    # the data is a mosaic of summer months
+    start_month = 6
+    # TODO: check if we should benchmark the 200m
+    # data too
+    input_height_width = 6
+
+    def __init__(self, mode: str = "s2", split: str = "train"):
+        assert mode in ["s2", "s1", "combined"]
+        self.mode = mode
+        self.split = split
+        self.masks = self.make_masks()
+
+        with (data_dir / treesat_dir / f"{split}_filenames.lst").open("r") as f:
+            self.images = [line for line in f]
+
+        with (data_dir / treesat_dir / labels_file).open("r") as f:
+            self.labels_dict = json.load(f)
+
+    def train_val_split(self, val_ratio: float = 0.1, seed=None):
+        if seed is not None:
+            random.seed(seed)
+        random.shuffle(self.images)
+        val_ds = deepcopy(self)
+        num_val = int(len(self.images) * val_ratio)
+        val_ds.images = self.images[:num_val]
+        self.images = self.images[num_val:]
+        return self, val_ds
+
+    @staticmethod
+    def image_name_to_paths(tif_file: str) -> Tuple[Path, Path]:
+        s1_path = data_dir / treesat_dir / s1_files_dir / Path(tif_file).name
+        s2_path = data_dir / treesat_dir / s2_files_dir / Path(tif_file).name
+        return s1_path, s2_path
+
+    def image_to_dynamic_eo_array(self, tif_file: str):
+        s1_image, s2_image = self.image_name_to_paths(tif_file)
+        s2 = rioxarray.open_rasterio(s2_image)
+        s1 = rioxarray.open_rasterio(s1_image)
+
+        kept_treesat_s2_band_idx = [i for i, val in enumerate(S2_BAND_ORDERING) if val in S2_BANDS]
+        kept_kept_treesat_s2_band_names = [val for val in S2_BAND_ORDERING if val in S2_BANDS]
+        treesat_to_cropharvest_s2_map = [
+            DYNAMIC_BANDS.index(val) for val in kept_kept_treesat_s2_band_names
+        ]
+
+        kept_treesat_s1_band_idx = [i for i, val in enumerate(S1_BAND_ORDERING) if val in S1_BANDS]
+        kept_kept_treesat_s1_band_names = [val for val in S1_BAND_ORDERING if val in S1_BANDS]
+        treesat_to_cropharvest_s1_map = [
+            DYNAMIC_BANDS.index(val) for val in kept_kept_treesat_s1_band_names
+        ]
+
+        labels_np = np.zeros(len(self.labels_to_int))
+        positive_classes = self.labels_dict[tif_file]
+        for name, percentage in positive_classes:
+            labels_np[self.labels_to_int[name]] = percentage
+
+        assert len(s1.x) == len(s1.y) == len(s2.x) == len(s2.y) == self.input_height_width
+        d_x = np.zeros([len(DYNAMIC_BANDS), self.input_height_width, self.input_height_width])
+        d_x[treesat_to_cropharvest_s2_map] = s2.values[kept_treesat_s2_band_idx]
+        d_x[treesat_to_cropharvest_s1_map] = s1.values[kept_treesat_s1_band_idx]
+
+        d_x = repeat(d_x, "c h w -> h w t c", t=self.num_timesteps)
+
+        return d_x, self.min_threshold(labels_np)
+
+    @staticmethod
+    def min_threshold(labels: np.ndarray, binarize: bool = True):
+        # this is what is also done in
+        # https://git.tu-berlin.de/rsim/treesat_benchmark/-/blob/master/TreeSat_Benchmark/trainers/utils.py#L27
+        lower_bound = 0.07  # anything below this is ignored
+        bounded = np.where(
+            labels > lower_bound,
+            np.ones_like(lower_bound) if binarize else labels,
+            np.zeros_like(lower_bound),
+        )
+        return bounded
+
+    def make_masks(self):
+        if self.mode == "s2":
+            dynamic_channels = [
+                idx for idx, key in enumerate(DYNAMIC_BANDS_GROUPS_IDX) if "S2" in key
+            ]
+
+        elif self.mode == "s1":
+            dynamic_channels = [
+                idx for idx, key in enumerate(DYNAMIC_BANDS_GROUPS_IDX) if "S1" in key
+            ]
+        else:
+            dynamic_channels = [
+                idx for idx, key in enumerate(DYNAMIC_BANDS_GROUPS_IDX) if "S" in key
+            ]
+
+        # everything is masked by default
+        dynamic_mask = np.ones([len(DYNAMIC_BANDS_GROUPS_IDX)])
+        # unmask available s2 bands
+        dynamic_mask[dynamic_channels] = 0
+        dynamic_mask = repeat(
+            dynamic_mask, "d -> h w t d", h=self.input_height_width, w=self.input_height_width, t=1
+        )
+
+        # no static channels are available
+        static_mask = np.ones(
+            [self.input_height_width, self.input_height_width, len(STATIC_BAND_GROUPS_IDX)]
+        )
+
+        assert ((dynamic_mask == 0) | (dynamic_mask == 1)).all()
+        assert (static_mask == 1).all()
+
+        return (dynamic_mask, static_mask)
+
+    def __getitem__(self, idx) -> Tuple[MaskedOutput, torch.Tensor]:
+        image = self.images[idx]
+        d_x, label = self.image_to_dynamic_eo_array(image.strip())
+
+        # static bands are not provided by eurosat
+        s_x = np.zeros((d_x.shape[0], d_x.shape[1], len(STATIC_BANDS)))
+
+        d_m, s_m = self.masks
+        month = np.ones((self.num_timesteps,)) * self.start_month
+
+        d_x_torch = torch.as_tensor(d_x, dtype=torch.float32)
+        s_x_torch = torch.as_tensor(s_x, dtype=torch.float32)
+        d_m_torch = torch.as_tensor(d_m, dtype=torch.float32)
+        s_m_torch = torch.as_tensor(s_m, dtype=torch.float32)
+        month_torch = torch.as_tensor(month, dtype=torch.long)
+        label_torch = torch.as_tensor(label, dtype=torch.long)
+
+        return (MaskedOutput(d_x_torch, s_x_torch, d_m_torch, s_m_torch, month_torch), label_torch)
+
+    def __len__(self):
+        return len(self.images)
