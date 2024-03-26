2b46396626c0931095baf3d28eec46ee1b2ca5c2
Gabriel Tseng <gabriel.tseng@mail.mcgill.ca>
2024-03-26 15:30:40 +0800

    type hints

--- src/data/earthengine/dynamic_world.py
+++ src/data/earthengine/dynamic_world.py
@@ -21,7 +21,7 @@ DW_DIV_VALUES = [1] * len(DW_BANDS)
 
 def get_single_dw_image(
     region: ee.Geometry, start_date: date, end_date: date
-) -> ee.ImageCollection:
+) -> ee.Image:
     # we start by getting all the data for the range
     dw_collection = (
         ee.ImageCollection("GOOGLE/DYNAMICWORLD/V1")
