47ec3885b9c72d923f0e6b07ce8072d1863cb7f4
marlens123 <marlena@itdex.de>
2024-04-01 12:36:51 -0400

    add evaltask

--- src/eval/so2sat_eval.py
+++ src/eval/so2sat_eval.py
@@ -1,10 +1,14 @@
-from typing import Tuple
+from typing import Dict, List, Optional, Sequence, Tuple
 
 import h5py
 import numpy as np
 import torch.multiprocessing
 from einops import repeat
+from sklearn.base import BaseEstimator
+from sklearn.metrics import accuracy_score
+from torch.utils.data import DataLoader
 from torch.utils.data import Dataset as PyTorchDataset
+from tqdm import tqdm
 
 from ..data.dataset import (
     DYNAMIC_BANDS,
@@ -15,8 +19,10 @@ from ..data.dataset import (
     normalize_dynamic,
 )
 from ..data.earthengine.s2 import S2_BANDS
+from ..flexipresto import Encoder
 from ..masked_datasets import MaskedOutput
-from ..utils import data_dir
+from ..utils import DEFAULT_SEED, data_dir, device
+from .eval import EvalTask, Hyperparams, model_class_name
 
 torch.multiprocessing.set_sharing_strategy("file_system")
 
@@ -133,3 +139,78 @@ class So2SatDataset(PyTorchDataset):
             with h5py.File(data_dir / self.so2sat_dir / f"{self.split}.h5", "r") as data:
                 self._len = data["sen1"].shape[0]
         return self._len
+
+
+class So2SatEval(EvalTask):
+    name = "so2sat"
+    regression = False
+    multilabel = False
+
+    def __init__(self, rgb: bool = True, patch_size: int = 8, seed=DEFAULT_SEED):
+        self.rgb = rgb
+        super().__init__(patch_size, seed)
+        self.name = f"{self.name}_{self.rgb}"
+
+    def compute_metrics(self, model_name: str, preds: np.ndarray, target: np.ndarray) -> Dict:
+        return {
+            f"{self.name}: {model_name}_accuracy_score": accuracy_score(target, preds),
+        }
+
+    @torch.no_grad()
+    def _evaluate_model(
+        self, pretrained_model: Encoder, sklearn_models: Sequence[BaseEstimator]
+    ) -> Dict:
+        test_dl = DataLoader(
+            So2SatDataset(split="testing"),
+            batch_size=Hyperparams.batch_size,
+            shuffle=False,
+            num_workers=Hyperparams.num_workers,
+        )
+        pred_dict: Dict[str, BaseEstimator] = {
+            model_class_name(model): [] for model in sklearn_models
+        }
+
+        labels_list = []
+
+        for masked_output, label in tqdm(test_dl, desc="Computing test predictions"):
+            d_x, s_x, d_m, s_m, months = [t.to(device) for t in masked_output]
+
+            pretrained_model.eval()
+
+            with torch.no_grad():
+                d_x, s_x, d_m, s_m, _ = pretrained_model(
+                    d_x, s_x, d_m, s_m, months, patch_size=self.patch_size
+                )
+                encodings = pretrained_model.average_tokens(d_x, s_x, d_m, s_m).cpu().numpy()
+
+            labels_list.append(label.cpu().numpy())
+
+            for model in sklearn_models:
+                preds = model.predict(encodings)
+                pred_dict[model_class_name(model)].append(preds)
+
+        target = np.concatenate(labels_list)
+        results_dict = {}
+
+        for model_name_str, pred_list in pred_dict.items():
+            test_preds_np = np.concatenate(pred_list, axis=0)
+            prefix = f"{model_name_str}"
+            results_dict.update(self.compute_metrics(prefix, test_preds_np, target))
+        return results_dict
+
+    def evaluate_model_on_task(
+        self, pretrained_model: Encoder, model_modes: Optional[List[str]] = None
+    ) -> Dict:
+        if model_modes is None:
+            model_modes = self.all_classification_sklearn_models
+        for model_mode in model_modes:
+            assert model_mode in self.all_classification_sklearn_models
+
+        train_dl = DataLoader(
+            So2SatDataset(split="training"),
+            batch_size=Hyperparams.batch_size,
+            shuffle=True,
+            num_workers=Hyperparams.num_workers,
+        )
+        trained_sklearn_models = self.train_sklearn_model(train_dl, pretrained_model, model_modes)
+        return self._evaluate_model(pretrained_model, trained_sklearn_models)
