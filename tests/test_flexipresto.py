13daa6b7924621df95d24027d3d61e54d3650b78
Gabriel Tseng <gabriel.tseng@mail.mcgill.ca>
2024-04-02 17:43:40 +0700

    :dog:

--- tests/test_flexipresto.py
+++ tests/test_flexipresto.py
@@ -3,8 +3,8 @@ from pathlib import Path
 
 import torch
 
-from src.data.config import NUM_TIMESTEPS
 from src.data import DYNAMIC_BANDS_GROUPS_IDX, Dataset
+from src.data.config import NUM_TIMESTEPS
 from src.data.dataset import DYNAMIC_BANDS, STATIC_BAND_GROUPS_IDX, STATIC_BANDS, DatasetOutput
 from src.flexipresto import Encoder, PrestoPixelDecoder, PrestoRepresentationDecoder
 from src.masking import batch_mask_presto, subset_batch_of_images
