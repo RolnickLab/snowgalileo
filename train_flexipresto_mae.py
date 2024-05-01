0707a4aa9233dbb8824976cd872ee5f31f6607b6
Gabriel Tseng <gabriel.tseng@mail.mcgill.ca>
2024-05-01 16:03:35 +0800

    Add China to eval tasks

--- train_flexipresto_mae.py
+++ train_flexipresto_mae.py
@@ -256,7 +256,7 @@ eval_tasks: List[EvalTask] = [
     *[TreeSatEval(mode, patch_size) for mode in ["s1", "s2", "combined"] for patch_size in [6, 3]],
     *[EuroSatEval(rgb) for rgb in [True, False]],
     So2SatEval(),
-    *[CropHarvestEval(country=country) for country in ["Kenya", "Togo", "Brazil"]],
+    *[CropHarvestEval(country=country) for country in ["Kenya", "Togo", "Brazil", "China"]],
 ]
 for task in eval_tasks:
     results = task.evaluate_model_on_task(encoder)
