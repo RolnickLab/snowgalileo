c8f27402ed62a232ced05a150e724bfca433acb7
Gabriel Tseng <gabriel.tseng@mail.mcgill.ca>
2024-05-01 20:40:15 +0800

    Add new export dataset

--- export.py
+++ export.py
@@ -8,6 +8,7 @@ import geopandas
 
 from src.data import EarthEngineExporter
 from src.data.config import DATA_FOLDER, EE_PROJECT
+from src.utils import DEFAULT_SEED
 
 os.environ["GOOGLE_CLOUD_PROJECT"] = EE_PROJECT
 
@@ -15,11 +16,16 @@ os.environ["GOOGLE_CLOUD_PROJECT"] = EE_PROJECT
 argparser = argparse.ArgumentParser()
 argparser.add_argument("--start_export_from_idx", type=int, default=0)
 argparser.add_argument("--num_exports", type=int, default=3000)
-argparser.add_argument("--filename", type=str, default="worldstrat_metadata.csv")
+argparser.add_argument("--filename", type=str, default="glance_locations_only.geojson")
 args = argparser.parse_args().__dict__
 
 filepath = DATA_FOLDER / "pretraining_points" / args["filename"]
 assert filepath.exists()
-latlons = geopandas.read_file(filepath)[args["start_export_from_idx"] :]
+latlons = geopandas.read_file(filepath).sample(frac=1, random_state=DEFAULT_SEED)
+
+if "lat" not in latlons.columns():
+    latlons["lon"] = latlons.geometry.centroid.x.values
+    latlons["lat"] = latlons.geometry.centroid.y.values
+
 exporter = EarthEngineExporter(check_gcp=True)
-exporter.export_for_latlons(latlons, args["num_exports"])
+exporter.export_for_latlons(latlons[args["start_export_from_idx"] :], args["num_exports"])
