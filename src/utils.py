5d552caa360b24700461b9efdea8a5a33d22af9b
Gabriel Tseng <gabriel.tseng@mail.mcgill.ca>
2024-06-04 09:00:09 -1000

    Implement gradient accumulation

--- src/utils.py
+++ src/utils.py
@@ -74,7 +74,7 @@ class AverageMeter:
 
 
 def load_check_config(name: str, mode: str):
-    assert mode in ["mae", "jepa"]
+    assert mode == "mae"
 
     with (config_dir / mode / name).open("r") as f:
         config = json.load(f)
@@ -82,6 +82,7 @@ def load_check_config(name: str, mode: str):
     expected_training_keys_type = {
         "num_epochs": int,
         "batch_size": int,
+        "effective_batch_size": int,
         "mask_ratio": float,
         "patch_sizes": list,
         "start_lr": float,
@@ -98,8 +99,6 @@ def load_check_config(name: str, mode: str):
         "patch_sizes_to_wandb_plot": list,
         "shape_time_combinations": list,
     }
-    if mode == "jepa":
-        expected_training_keys_type["ema"] = list
     training_dict = config["training"]
 
     for key, val in expected_training_keys_type.items():
