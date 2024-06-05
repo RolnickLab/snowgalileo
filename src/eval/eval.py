d96547dbecb2d13b241a6a0387931a351e1867ff
marlens123 <marlena@itdex.de>
2024-06-05 14:08:31 -0400

    test targets per token

--- src/eval/eval.py
+++ src/eval/eval.py
@@ -13,6 +13,8 @@ from torch import nn
 from torch.optim import AdamW
 from torch.utils.data import DataLoader
 from tqdm import tqdm
+from einops import rearrange
+from collections import Counter
 
 from ..flexipresto import Encoder, FinetuningHead, PrestoFineTuningModel
 from ..utils import DEFAULT_SEED, device
@@ -23,7 +25,7 @@ logger = logging.getLogger("__main__")
 
 @dataclass
 class Hyperparams:
-    batch_size: int = 8
+    batch_size: int = 128
     num_workers: int = 0
     max_epochs: int = 1
     patience: int = 10
@@ -80,6 +82,56 @@ class EvalTask(ABC):
         return model
 
     @torch.no_grad()
+    def group_and_reduce_targets_per_token(self, target: torch.Tensor, mode: str = "one-target-per-token") -> torch.Tensor:
+        # group labels per token for segmentation and reduce their dimensionality
+        # grouped_label shape will be (batch_size, n_tokens, t_height * t_width)
+        grouped_label = (
+            target.reshape(
+                target.shape[0],
+                target.shape[1] // self.patch_size,
+                self.patch_size,
+                target.shape[2] // self.patch_size,
+                self.patch_size,
+            )
+            .permute(0, 1, 3, 2, 4)
+            .reshape(target.shape[0], -1, self.patch_size * self.patch_size)
+        )
+        if mode == "one-target-per-token":
+            # take the most common label per token
+            label = rearrange(grouped_label.mode(dim=2).values, "b n_t -> (b n_t)")
+
+        elif mode == "unique-targets-per-token":
+            # print number of unique labels per token
+            flattened_labels = rearrange(grouped_label, "b n_t n -> (b n_t) n")
+            length = []
+            for token in flattened_labels:
+                length.append(len(Counter(token.cpu().numpy())))
+                #print(Counter(token.cpu().numpy()))
+            # get the mean length
+            print(np.mean(np.array(length)))
+
+            # TODO: implement this
+            # one-hot encode classes for each token, resulting in a tensor of shape (batch_size, n_tokens, n_classes)
+
+            # label = torch.zeros(grouped_label.shape[0], grouped_label.shape[1], self.num_outputs, dtype=torch.long, device=device)
+            # label = rearrange(grouped_label.mode(dim=2).values, "b n_t n_cl -> (b n_t) n_cl")
+
+        elif mode == "all-targets-per-token":
+            label = rearrange(grouped_label, "b n_t h w -> (b n_t) (h w)")
+        return length
+    
+    @torch.no_grad()
+    def group_encodings_per_token(
+        self, model, s_t_x, s_x, t_x, s_t_m, s_m, t_m
+    ) -> np.ndarray:
+        encodings = rearrange(
+            model.apply_mask_and_average_tokens_per_patch(s_t_x, s_x, t_x, s_t_m, s_m, t_m),
+            "b n_t n_f -> (b n_t) n_f",
+        )
+        return encodings
+    
+
+    @torch.no_grad()
     def train_sklearn_model(
         self,
         train_dl: DataLoader,
@@ -97,16 +149,37 @@ class EvalTask(ABC):
 
         for masked_output, label in tqdm(train_dl, desc="Computing encodings for sklearn"):
             s_t_x, s_x, t_x, s_t_m, s_m, t_m, months = [t.to(device) for t in masked_output]
-            target_list.append(label.cpu().numpy())
+
+            if self.segmentation:
+                target_list.append(self.group_and_reduce_targets_per_token(label).cpu().numpy())
+            else:
+                target_list.append(label.cpu().numpy())
+
             with torch.no_grad():
                 s_t_x, s_x, t_x, s_t_m, s_m, t_m, _ = pretrained_model(
                     s_t_x, s_x, t_x, s_t_m, s_m, t_m, months, patch_size=self.patch_size
                 )
-                encodings = (
-                    pretrained_model.average_tokens(s_t_x, s_x, t_x, s_t_m, s_m, t_m).cpu().numpy()
-                )
-                encoding_list.append(encodings)
+                if self.segmentation:
+                    encoding_list.append(
+                        self.group_encodings_per_token(
+                            pretrained_model, s_t_x, s_x, t_x, s_t_m, s_m, t_m
+                        )
+                        .cpu()
+                        .numpy()
+                    )
+                else:
+                    encoding_list.append(
+                        pretrained_model.average_tokens(s_t_x, s_x, t_x, s_t_m, s_m, t_m)
+                        .cpu()
+                        .numpy()
+                    )
 
+        # TODO: implement stratified sampling
+        # do stratified sampling (10% of all vectors), sample by keeping the same 
+        # class balance as the original dataset or have it class-balanced
+        # ideally don't compute unnecessary encodings
+        # only targets need to be stratified
+        
         encodings_np = np.concatenate(encoding_list)
         targets = np.concatenate(target_list)
         if len(targets.shape) == 2 and targets.shape[1] == 1:
--- src/flexipresto.py
+++ src/flexipresto.py
@@ -660,6 +660,34 @@ class Encoder(FlexiPrestoBase):
         x, _, m = cls.collapse_and_combine_hwtc(s_t_x, s_x, t_x, s_t_m, s_m, t_m)
         x_for_mean = x * (1 - m.unsqueeze(-1))
         return x_for_mean.sum(dim=1) / torch.sum(1 - m, -1, keepdim=True)
+    
+    @classmethod
+    def apply_mask_and_average_tokens_per_patch(
+        cls,
+        s_t_x: torch.Tensor,
+        s_x: torch.Tensor,
+        t_x: torch.Tensor,
+        s_t_m: torch.Tensor,
+        s_m: torch.Tensor,
+        t_m: torch.Tensor,
+    ):
+        s_t_x = rearrange(s_t_x, "b t_h t_w t c_g d -> b (t_h t_w) (t c_g) d")
+        s_x = rearrange(s_x, "b t_h t_w c_g d -> b (t_h t_w) c_g d")
+        # repeat time tokens over space
+        t_x = repeat(
+            rearrange(t_x, "b t c_g d -> b (t c_g) d"), "b n d -> b s n d", s=s_x.shape[1]
+        )
+        s_t_m = rearrange(s_t_m, "b t_h t_w t c_g-> b (t_h t_w) (t c_g)")
+        s_m = rearrange(s_m, "b t_h t_w c_g-> b (t_h t_w) c_g")
+        t_m = repeat(rearrange(t_m, "b t c_g -> b (t c_g)"), "b n -> b s n", s=s_x.shape[1])
+
+        x = torch.cat([s_t_x, s_x, t_x], dim=2)  # B, S, N, D
+        m = torch.cat([s_t_m, s_m, t_m], dim=2)  # B, S, N
+
+        x_for_mean = x * (1 - m.unsqueeze(-1))
+
+        return x_for_mean.sum(dim=2) / torch.sum(1 - m, -1, keepdim=True)
+    
 
     def forward(
         self,
@@ -885,7 +913,7 @@ class PrestoRepresentationDecoder(FlexiPrestoBase):
         )
 
 
-class FinetuningHead(nn.Module):
+class FinetuningHead(FlexiPrestoBase):
     def __init__(
         self,
         num_outputs: int,
