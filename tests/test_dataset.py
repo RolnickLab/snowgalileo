5f6a6b80243b471f41aa4a9eea21232993fe7592
Gabriel Tseng <gabriel.tseng@mail.mcgill.ca>
2024-06-13 14:40:34 -0700

    Is this enough?

--- tests/test_dataset.py
+++ tests/test_dataset.py
@@ -5,11 +5,13 @@ import numpy as np
 
 from src.data.dataset import SPACE_BANDS, SPACE_TIME_BANDS, STATIC_BANDS, TIME_BANDS, Dataset
 
+BROKEN_FILE = "min_lat=24.7979_min_lon=-105.1508_max_lat=24.8069_max_lon=-105.141_dates=2022-01-01_2023-12-31.tif"
 TEST_FILENAMES = [
     "min_lat=5.4427_min_lon=101.4016_max_lat=5.4518_max_lon=101.4107_dates=2022-01-01_2023-12-31.tif",
     "min_lat=-27.6721_min_lon=25.6796_max_lat=-27.663_max_lon=25.6897_dates=2022-01-01_2023-12-31.tif",
 ]
-TEST_FILES = [Path(__file__).parents[1] / f"data/tifs/{x}" for x in TEST_FILENAMES]
+TIFS_FOLDER = Path(__file__).parents[1] / "data/tifs"
+TEST_FILES = [TIFS_FOLDER / x for x in TEST_FILENAMES]
 
 
 class TestDataset(unittest.TestCase):
@@ -32,6 +34,14 @@ class TestDataset(unittest.TestCase):
             self.assertEqual(len(TIME_BANDS), t_x.shape[-1])
             self.assertEqual(len(STATIC_BANDS), st_x.shape[-1])
             self.assertEqual(months[0], 0)
+
+    def test_files_are_replaced(self):
+        ds = Dataset(TIFS_FOLDER, download=False)
+        assert TIFS_FOLDER / BROKEN_FILE in ds.tifs
+
+        for b in ds:
+            assert len(b) == 5
+        assert TIFS_FOLDER / BROKEN_FILE not in ds.tifs
 
     def test_subset_image_with_minimum_size(self):
         input = np.ones((3, 3, 1))
