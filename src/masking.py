Fix masking

src/masking.py
@@ -315,8 +315,9 @@ def batch_mask_time(
             assert bands_to_keep is not None
             space_time_mask[:, :, :, :, bands_to_keep] = 0
         else:
-            space_time_mask[:, :, :, b_flat_timesteps_t == 0, bands_to_keep] = 0
-            space_time_mask[:, :, :, b_flat_timesteps_t == 0, bands_to_mask] = 1
+            expanded_timesteps = repeat(b_flat_timesteps_t, "b t-> b h w t", h=h, w=w).clone()
+            space_time_mask[expanded_timesteps == 0, bands_to_keep] = 0
+            space_time_mask[expanded_timesteps == 0, bands_to_mask] = 1
     if bands_to_decode is not None:
         # ignore all previous calculations about what should be decoded
         space_time_mask = torch.clamp(space_time_mask, max=1)

tests/test_masking.py
@@ -76,7 +76,7 @@ class TestMasking(unittest.TestCase):
                         )  # b, t
                         time_mask_along_t = output.time_mask.float().mean(axis=-1)  # b, t
                         self.assertTrue(torch.equal(space_time_mask_along_t, time_mask_along_t))
-                        self.assertTrue(np.isin(space_time_mask_along_t, (0, 1)).all())
+                        self.assertTrue(np.isin(space_time_mask_along_t, (0, 1, 2)).all())
                         self.assertTrue(
                             (
                                 space_time_mask_along_t.sum(axis=1)