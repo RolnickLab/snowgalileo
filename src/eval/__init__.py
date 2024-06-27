3aac1af8cb3f08100959400e4ae54546aae9e987
marlens123 <marlena@itdex.de>
2024-06-27 12:40:27 -0400

    oops

--- src/eval/__init__.py
+++ src/eval/__init__.py
@@ -1,11 +1,10 @@
-from sa_crop_type_eval import SACropEval
-
 from .bigearthnet_eval import BigEarthNetEval
 from .brick_kiln_eval import BrickKilnEval
 from .cashew_plant_eval import CashewPlantEval
 from .cropharvest_eval import BinaryCropHarvestEval, MultiClassCropHarvestEval
 from .eurosat_eval import EuroSatEval
 from .pastis_pixel_eval import PastisPixelEval
+from .sa_crop_type_eval import SACropEval
 from .so2sat_eval import So2SatEval
 from .treesat_eval import TreeSatEval
 
