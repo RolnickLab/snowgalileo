b610dd705126029a6ea083513b06dc3bf5aa1f45
marlens123 <marlena@itdex.de>
2024-06-27 09:48:44 -0400

    cashew and sa crop type eval

--- src/eval/__init__.py
+++ src/eval/__init__.py
@@ -1,6 +1,9 @@
+from sa_crop_type_eval import SACropEval
+
+from .cashew_plantation_eval import CashewEval
 from .cropharvest_eval import BinaryCropHarvestEval, MultiClassCropHarvestEval
 from .eurosat_eval import EuroSatEval
-from .pastis_pixel_eval import PastisEval
+from .pastis_pixel_eval import PastisPixelEval
 from .so2sat_eval import So2SatEval
 from .treesat_eval import TreeSatEval
 
@@ -10,5 +13,7 @@ __all__ = [
     "So2SatEval",
     "BinaryCropHarvestEval",
     "MultiClassCropHarvestEval",
-    "PastisEval",
+    "PastisPixelEval",
+    "CashewEval",
+    "SACropEval",
 ]
