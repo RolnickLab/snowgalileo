Fix decoder time masking

src/masking.py
@@ -319,8 +319,8 @@ def batch_mask_time(
     _, bands_to_mask = check_mode_and_return_channels(mode)
     targeted_bands_to_decode = check_unmasking_mode_and_return_channels(decoder_mode)
     # if there is only a single timestep, decode it
-    num_timesteps_to_decode = max(t * decoder_unmask_ratio, 1)
-    num_timesteps_to_encode = max(t * (1 - (mask_ratio + decoder_unmask_ratio)), 1)
+    num_timesteps_to_decode = max(int(t * decoder_unmask_ratio), 1)
+    num_timesteps_to_encode = max(int(t * mask_ratio), 1)
     # we do this as a numpy array to take advantage of
     # numpy's permuted function
     flat_timesteps = np.concatenate(
@@ -358,7 +358,7 @@ def batch_mask_time(
     static_mask = _random_mask_for_b(b, static_x.device, mask_ratio, decoder_unmask_ratio)
     static_mask = repeat(static_mask, "b -> b c_g", c_g=len(STATIC_BAND_GROUPS_IDX))
 
-    if bands_to_mask is not None:
+    if bands_to_mask is not None:  # mode != random
         space_time_mask[:, :, :, :, bands_to_mask] = torch.clamp(
             space_time_mask[:, :, :, :, bands_to_mask], min=1
         )
@@ -366,7 +366,7 @@ def batch_mask_time(
         time_mask = torch.clamp(time_mask, min=1)
         static_mask = torch.clamp(static_mask, min=1)
 
-    if targeted_bands_to_decode is not None:
+    if targeted_bands_to_decode is not None:  # decoder mode != random
         # ignore all previous calculations about what should be decoded
         space_time_mask = torch.clamp(space_time_mask, max=1)
         space_mask = torch.clamp(space_mask, max=1)

tests/test_masking.py
@@ -27,7 +27,6 @@ from src.masking import (
     batch_mask_space,
     batch_mask_time,
     batch_subset_mask_presto_augmented,
-    round_school,
 )
 
 MASK_TO_BANDS = {
@@ -62,7 +61,7 @@ class TestMasking(unittest.TestCase):
         )
 
     def test_mask_by_time(self):
-        for t in range(1, 8):
+        for t in range(4, 8):
             b, h, w = 2, 16, 16
             space_time_input = torch.ones((b, h, w, t, 8))
             space_input = torch.ones((b, h, w, 8))
@@ -84,6 +83,7 @@ class TestMasking(unittest.TestCase):
                         decoder_unmask_ratio=decoder_unmask_ratio,
                         mode=mode,
                         decoder_mode=decoder_mode,
+                        patch_size=4,
                     )
                     self.check_all_values_in_masks(
                         output.space_time_mask,
@@ -103,7 +103,8 @@ class TestMasking(unittest.TestCase):
 
                     # the branching in the test below is a bit ugly and could be more concise,
                     # but I think it does test for all combinations
-                    expected_unmasked_timesteps = round_school(t * mask_ratio) if t > 1 else 1
+                    expected_unmasked_timesteps = max(int(t * mask_ratio), 1)
+                    expected_decoder_timesteps = max(int(t * decoder_unmask_ratio), 1)
                     if (mode == "random") and (decoder_mode == "random"):
                         # collapse the dynamic_mask along the time dimension
                         space_time_mask_along_t = output.space_time_mask.float().mean(
@@ -176,8 +177,7 @@ class TestMasking(unittest.TestCase):
                             self.assertTrue(
                                 (
                                     (space_time_mask_along_t == 2).sum(axis=1)
-                                    / space_time_mask_along_t.shape[1]
-                                    == (decoder_unmask_ratio if t > 1 else 1)
+                                    == expected_decoder_timesteps
                                 ).all()
                             )
 