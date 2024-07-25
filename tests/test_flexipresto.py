a881d3b8e87a062fb5be8ceda3d03d1de56cece0
Gabriel Tseng <gabriel.tseng@mail.mcgill.ca>
2024-07-25 15:27:46 -0700

    o em gee

--- tests/test_flexipresto.py
+++ tests/test_flexipresto.py
@@ -23,6 +23,7 @@ from src.masking import (
     STATIC_BAND_EXPANSION,
     TIME_BAND_EXPANSION,
     MaskingFunctions,
+    batch_mask_space,
     batch_subset_mask_presto,
 )
 from src.utils import device, load_check_config
@@ -358,3 +359,52 @@ class TestPresto(unittest.TestCase):
 
         for key, val in new_encoder.state_dict().items():
             self.assertTrue(torch.equal(val, original_encoder.state_dict()[key]))
+
+    def test_decoder_and_mask(self):
+        patch_size = 4
+        mask_ratio = 0.25
+        decoder_unmask_ratio = 0.25
+
+        ds = Dataset(DATA_FOLDER, False)
+        s_t_x, sp_x, t_x, st_x, months = self.to_tensor_with_batch_d(ds[0])
+        masked_output = batch_mask_space(
+            s_t_x,
+            sp_x,
+            t_x,
+            st_x,
+            months,
+            mask_ratio=mask_ratio,
+            decoder_unmask_ratio=decoder_unmask_ratio,
+            mode=[
+                ("space_time", "S2_RGB"),
+                ("space_time", "S2_SWIR"),
+                ("space_time", "S2_Red_Edge"),
+                ("space_time", "S2_NIR_10m"),
+                ("space_time", "S2_NIR_20m"),
+            ],
+            decoder_mode=[("static", "LS")],
+            patch_size=patch_size,
+        )
+
+        encoder = Encoder(embedding_size=32, num_heads=1)
+        decoder = PrestoPixelDecoder(
+            encoder_embedding_size=32,
+            decoder_embedding_size=32,
+            num_heads=1,
+        )
+        encoder_output = encoder(
+            masked_output.space_time_x,
+            masked_output.space_x,
+            masked_output.time_x,
+            masked_output.static_x,
+            masked_output.space_time_mask,
+            masked_output.space_mask,
+            masked_output.time_mask,
+            masked_output.static_mask,
+            masked_output.months.long(),
+            patch_size=patch_size,
+        )
+        s_t_x, sp_x, t_x, st_x, s_t_m, sp_m, t_m, st_m, _ = encoder_output
+        x, m = decoder.collapse_and_combine_hwtc(s_t_x, sp_x, t_x, st_x, s_t_m, sp_m, t_m, st_m)
+        x, _, _, _, _ = decoder.split_x_y(x, m)
+        self.assertTrue(x.shape[1] == 1, x.shape)
