de4d722a031fb07e83486f58768dbccb8f34205c
anthony <anthonydavidfuller@gmail.com>
2024-08-08 19:47:57 -0400

    all tokens works well!

--- train_flexipresto_mae.py
+++ train_flexipresto_mae.py
@@ -39,7 +39,7 @@ from src.eval import (
 )
 from src.eval.eval import EvalTask, Hyperparams
 from src.flexipresto import Encoder, PrestoPixelDecoder, adjust_learning_rate
-from src.loss import mse_loss, patch_disc_loss_slow, patch_disc_loss
+from src.loss import mse_loss, patch_disc_loss_slow, patch_disc_loss_v2
 from src.utils import (
     AverageMeter,
     data_dir,
@@ -242,7 +242,22 @@ for e in tqdm(range(training_config["num_epochs"])):
                     t_sp = encoder.blocks[0].norm1(t_sp)
                     t_st = encoder.blocks[0].norm1(t_st)
 
-                loss = mse_loss(
+                # loss = patch_disc_loss_slow(
+                #     t_s_t,
+                #     t_sp,
+                #     t_t,
+                #     t_st,
+                #     p_s_t,
+                #     p_sp,
+                #     p_t,
+                #     p_st,
+                #     s_t_m[:, 0::patch_size, 0::patch_size],
+                #     sp_m[:, 0::patch_size, 0::patch_size],
+                #     t_m,
+                #     st_m,
+                # )
+
+                loss = patch_disc_loss_v2(
                     t_s_t,
                     t_sp,
                     t_t,
@@ -256,6 +271,8 @@ for e in tqdm(range(training_config["num_epochs"])):
                     t_m,
                     st_m,
                 )
+                # print(loss, loss_fast)
+                # exit()
 
             train_loss.update(loss.item(), n=s_t_x.shape[0])
             if c_i is not None:
