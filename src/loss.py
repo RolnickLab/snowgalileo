90d5411b4216bbfba79e8aaf0745f902c9e00907
anthony <anthonydavidfuller@gmail.com>
2024-08-09 10:13:43 -0400

    lets go

--- src/loss.py
+++ src/loss.py
@@ -50,6 +50,11 @@ def seq_and_cat(s_t, sp, t, st):
     return torch.cat([s_t, sp, t, st], dim=1)
 
 
+def expand_and_reciprocate(t):
+    reciprocals = torch.reciprocal(t.float())
+    return torch.repeat_interleave(reciprocals, t)
+
+
 def patch_disc_loss(
     t_s_t,
     t_sp,
@@ -66,94 +71,16 @@ def patch_disc_loss(
     pred2unit=True,
     tau=0.2,
 ):
-    # make targets 1, everything else 0
-    s_t_m = (s_t_m == 2).int().unsqueeze(dim=-1)
-    sp_m = (sp_m == 2).int().unsqueeze(dim=-1)
-    t_m = (t_m == 2).int().unsqueeze(dim=-1)
-    st_m = (st_m == 2).int().unsqueeze(dim=-1)
-
     # create tensors of shape (bsz, seq_len, dim)
-    all_masks = seq_and_cat(s_t_m, sp_m, t_m, st_m)
+    all_masks = seq_and_cat(s_t_m.unsqueeze(dim=-1), sp_m.unsqueeze(dim=-1), t_m.unsqueeze(dim=-1), st_m.unsqueeze(dim=-1)).squeeze(-1)
     all_preds = seq_and_cat(p_s_t, p_sp, p_t, p_st)
     all_targets = seq_and_cat(t_s_t, t_sp, t_t, t_st)
 
-    # create logit masks
-    all_masks_squeezed = all_masks.squeeze(-1)  # (bsz, seq_len)
-    row_mask = all_masks_squeezed.unsqueeze(2)  # (bsz, seq_len, 1)
-    col_mask = all_masks_squeezed.unsqueeze(1)  #  (bsz, 1, seq_len)
-    mask_intersection = row_mask & col_mask  #  (bsz, seq_len, seq_len)
-    logit_mask = ((mask_intersection)*-1 + 1) * -1_000_000
-
-    counts = torch.sqrt(mask_intersection.sum(dim=-1).sum(dim=-1))
-
-    bs, nt, d = all_preds.shape
-
-    all_preds = F.normalize(all_preds, p=2, dim=-1)
-    all_targets = F.normalize(all_targets, p=2, dim=-1)
-
-    scores = torch.einsum('npd,nqd->npq', all_preds, all_targets) / tau
-    scores_sm = (scores + logit_mask).softmax(dim=-1)
-
-    eyye = torch.eye(nt, dtype=torch.float, device=scores.device)[None].repeat(bs, 1, 1)
-
-    neg_log = -torch.log(scores_sm + 0.000001) * mask_intersection
-    custom_ce_loss = (tau * 2) * (neg_log * eyye).sum(dim=-1).sum(dim=-1) / counts
-    custom_ce_loss = custom_ce_loss.mean()
-    return custom_ce_loss
-
-
-def patch_disc_loss_v2(
-    t_s_t,
-    t_sp,
-    t_t,
-    t_st,
-    p_s_t,
-    p_sp,
-    p_t,
-    p_st,
-    s_t_m,
-    sp_m,
-    t_m,
-    st_m,
-    pred2unit=True,
-    tau=0.2,
-):
-    
-    # work-in-progress
-
-    p_s_t_selected = p_s_t[s_t_m == 2]
-    p_sp_selected = p_sp[sp_m == 2]
-    p_t_selected = p_t[t_m == 2]
-    p_st_selected = p_st[st_m == 2]
-
-    pred = torch.cat(
-        [
-            p_s_t_selected,
-            p_sp_selected,
-            p_t_selected,
-            p_st_selected
-
-        ], dim=0
-    ).unsqueeze(dim=0)
-
-    t_s_t_selected = t_s_t[s_t_m == 2]
-    t_sp_selected = t_sp[sp_m == 2]
-    t_t_selected = t_t[t_m == 2]
-    t_st_selected = t_st[st_m == 2]
-
-    target = torch.cat(
-        [
-            t_s_t_selected,
-            t_sp_selected,
-            t_t_selected,
-            t_st_selected
-
-        ], dim=0
-    ).unsqueeze(dim=0)
-
+    pred = all_preds[all_masks == 2].unsqueeze(dim=0)
+    target = all_targets[all_masks == 2].unsqueeze(dim=0)
 
     bs, nt, d = pred.shape
-    
+
     if pred2unit:
         pred_mu = pred.mean(1, keepdims=True)
         pred_std = pred.std(1, keepdims=True)
@@ -163,9 +90,21 @@ def patch_disc_loss_v2(
     target = F.normalize(target, p=2, dim=-1)
 
     scores = torch.einsum('npd,nqd->npq', pred, target) / tau
-    labels = torch.arange(nt, dtype=torch.long, device=pred.device)[None].repeat(bs, 1)
-    loss = F.cross_entropy(scores.flatten(0, 1), labels.flatten(0, 1)) * (tau * 2)
+    count = (all_masks == 2).sum(dim=-1)
 
+    logit_mask = torch.full_like(scores, -torch.finfo(scores.dtype).max)
+    start = 0
+    for c in count:
+        end = start + c
+        logit_mask[:, start:end, start:end] = 0
+        start += c
+
+    scores = scores + logit_mask
+
+    labels = torch.arange(nt, dtype=torch.long, device=pred.device)[None].repeat(bs, 1)
+    loss = F.cross_entropy(scores.flatten(0, 1), labels.flatten(0, 1), reduction='none') * (tau * 2)
+    loss_multiplier = expand_and_reciprocate(count)
+    loss = (loss * loss_multiplier).sum() / t_s_t.shape[0]
     return loss
 
 
@@ -186,35 +125,13 @@ def all_patch_disc_loss(
     tau=0.2,
 ):
 
-    p_s_t_selected = p_s_t[s_t_m == 2]
-    p_sp_selected = p_sp[sp_m == 2]
-    p_t_selected = p_t[t_m == 2]
-    p_st_selected = p_st[st_m == 2]
+    # create tensors of shape (bsz, seq_len, dim)
+    all_masks = seq_and_cat(s_t_m.unsqueeze(dim=-1), sp_m.unsqueeze(dim=-1), t_m.unsqueeze(dim=-1), st_m.unsqueeze(dim=-1)).squeeze(-1)
+    all_preds = seq_and_cat(p_s_t, p_sp, p_t, p_st)
+    all_targets = seq_and_cat(t_s_t, t_sp, t_t, t_st)
 
-    pred = torch.cat(
-        [
-            p_s_t_selected,
-            p_sp_selected,
-            p_t_selected,
-            p_st_selected
-
-        ], dim=0
-    ).unsqueeze(dim=0)
-
-    t_s_t_selected = t_s_t[s_t_m == 2]
-    t_sp_selected = t_sp[sp_m == 2]
-    t_t_selected = t_t[t_m == 2]
-    t_st_selected = t_st[st_m == 2]
-
-    target = torch.cat(
-        [
-            t_s_t_selected,
-            t_sp_selected,
-            t_t_selected,
-            t_st_selected
-
-        ], dim=0
-    ).unsqueeze(dim=0)
+    pred = all_preds[all_masks == 2].unsqueeze(dim=0)
+    target = all_targets[all_masks == 2].unsqueeze(dim=0)
 
     # remainder of code exactly like LatentMIM
     bs, nt, d = pred.shape
@@ -232,101 +149,3 @@ def all_patch_disc_loss(
     loss = F.cross_entropy(scores.flatten(0, 1), labels.flatten(0, 1)) * (tau * 2)
 
     return loss
-
-
-
-
-def remove_masks_and_cat(
-    batch_idx,
-    s_t,
-    sp,
-    t,
-    st,
-    s_t_m,
-    sp_m,
-    t_m,
-    st_m,
-):
-    s_t = rearrange(s_t[batch_idx], 'h w t c_g d -> 1 (h w t c_g) d')
-    sp = rearrange(sp[batch_idx], 'h w c_g d -> 1 (h w c_g) d')
-    t = rearrange(t[batch_idx], 't c_g d -> 1 (t c_g) d')
-    st = rearrange(st[batch_idx], 'c_g d -> 1 c_g d')
-
-    s_t_m = rearrange(s_t_m[batch_idx], "h w t c_g -> (h w t c_g)")
-    sp_m = rearrange(sp_m[batch_idx], "h w c_g -> (h w c_g)")
-    t_m = rearrange(t_m[batch_idx], "t c_g -> (t c_g)")
-    st_m = st_m[batch_idx]
-
-    return torch.cat(
-                [
-                    s_t[:, s_t_m == 2, :],
-                    sp[:, sp_m == 2, :],
-                    t[:, t_m == 2, :],
-                    st[:, st_m == 2, :],
-                ],
-            dim=1
-            )
-    
-    
-
-
-def patch_disc_loss_slow(
-    t_s_t,
-    t_sp,
-    t_t,
-    t_st,
-    p_s_t,
-    p_sp,
-    p_t,
-    p_st,
-    s_t_m,
-    sp_m,
-    t_m,
-    st_m,
-    pred2unit=True,
-    tau=0.2,
-):
-    bsz = t_s_t.shape[0]
-
-    loss = 0
-    for batch_idx in range(bsz):
-        pred = remove_masks_and_cat(
-            batch_idx,
-            p_s_t,
-            p_sp,
-            p_t,
-            p_st,
-            s_t_m,
-            sp_m,
-            t_m,
-            st_m,
-        )
-
-        target = remove_masks_and_cat(
-            batch_idx,
-            t_s_t,
-            t_sp,
-            t_t,
-            t_st,
-            s_t_m,
-            sp_m,
-            t_m,
-            st_m,
-        )
-
-        bs, nt, d = pred.shape
-        
-        # if pred2unit:
-        #     pred_mu = pred.mean(1, keepdims=True)
-        #     pred_std = pred.std(1, keepdims=True)
-        #     pred = (pred - pred_mu) / (pred_std + 1e-4)
-
-        pred = F.normalize(pred, p=2, dim=-1)
-        target = F.normalize(target, p=2, dim=-1)
-
-        scores = torch.einsum('npd,nqd->npq', pred, target) / tau
-
-        labels = torch.arange(nt, dtype=torch.long, device=pred.device)[None].repeat(bs, 1)
-        loss += F.cross_entropy(scores.flatten(0, 1), labels.flatten(0, 1)) * (tau * 2)
-    
-    return loss / bsz
