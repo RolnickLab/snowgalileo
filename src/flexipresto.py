ef58361ab74ea7638bc2f166ccf0acffa4cc3f3d
Gabriel Tseng <gabriel.tseng@mail.mcgill.ca>
2024-08-12 11:19:19 -0700

    :facepalm: thanks unit tests

--- src/flexipresto.py
+++ src/flexipresto.py
@@ -808,7 +808,7 @@ class Encoder(FlexiPrestoBase):
         x, indices, new_m = self.remove_masked_tokens(x, new_m)  # new_m is shape (bsz, seq_len)
 
         for i_blk, blk in enumerate(self.blocks):
-            if (i_blk + 1) >= exit_after:
+            if (exit_after is not None) and ((i_blk + 1) >= exit_after):
                 # if exit_after is N, then we exit after the Nth layer
                 # if exit_after is 0, then all layers are skipped
                 break
@@ -900,7 +900,7 @@ class Encoder(FlexiPrestoBase):
         ) = self.apply_linear_projection(
             s_t_x, sp_x, t_x, st_x, s_t_m, sp_m, t_m, st_m, patch_size
         )
-        if (exit_after is not None) and (exit_after > 0):
+        if (exit_after is None) or (exit_after > 0):
             s_t_x, sp_x, t_x, st_x, s_t_m, st_m, t_m, st_m = self.apply_attn(
                 s_t_x,
                 sp_x,
