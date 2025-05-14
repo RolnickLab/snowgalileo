f48623461b926e3161e43115508a157d67217692
marlens123 <marlena@itdex.de>
2025-05-14 09:14:08 -0400

    mypy

--- src/masking.py
+++ src/masking.py
@@ -2,7 +2,7 @@ import random
 from collections import OrderedDict
 from enum import Enum
 from itertools import chain, combinations, product
-from typing import Any, Callable, Dict, List, NamedTuple, Optional, Tuple
+from typing import Dict, List, NamedTuple, Optional, Tuple
 
 import numpy as np
 import torch
@@ -238,9 +238,7 @@ def batch_subset_mask_presto(
     assert len(masking_probabilities) == len(MASKING_MODES)
 
     if masking_function.value < 2:
-        f: Callable[..., Any] = (
-            batch_mask_space if masking_function.value == 1 else batch_mask_time
-        )
+        f = batch_mask_space if masking_function.value == 1 else batch_mask_time
         num_masking_modes = random.choice(list(range(2, MAX_MASKING_STRATEGIES + 1)))
         masking_modes = weighted_sample_without_replacement(
             MASKING_MODES, weights=masking_probabilities, k=num_masking_modes
