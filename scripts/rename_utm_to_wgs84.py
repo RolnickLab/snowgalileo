bd910d7a55cb2b24a50f63451548110fab3f53e7
marlens123 <marlena@itdex.de>
2025-09-14 22:25:02 -0400

    rename utm to wgs

---
+++ scripts/rename_utm_to_wgs84.py
@@ -0,0 +1,32 @@
+from src.data.config import DATA_FOLDER
+from pathlib import Path
+from sklearn.model_selection import train_test_split
+from src.config import DEFAULT_SEED
+import argparse
+import rasterio
+from pyproj import Transformer
+
+argparser = argparse.ArgumentParser()
+argparser.add_argument("--folder", type=str, default="landsat_eval_tifs/patches_UTM_1_99_test_cropped")
+
+args = argparser.parse_args().__dict__
+
+path = Path(DATA_FOLDER / args["folder"])
+
+# connvert all files in the folder from _UTM_ to _WGS84_ in their filename
+for file in path.glob("*.tif"):
+
+    with rasterio.open(file) as src:
+        crs = src.crs.to_string()
+
+    old_name = file.name
+    parts = old_name.split(".tif")[0].split("_")
+    utm_lat = float(parts[3])
+    utm_lon = float(parts[4])
+
+    # NOTE: always_xy=True ensures that the first coordinate is always in northerly direction
+    transformer = Transformer.from_crs(crs, "EPSG:4326", always_xy=True)
+    lon, lat = transformer.transform(utm_lon, utm_lat)
+
+    new_name = file.name.replace(str(utm_lon), str(lon)).replace(str(utm_lat), str(lat))
+    file.rename(path / new_name)
