fa0f596e2fd8f23fec3284e9de071fa2d94157b5
marlens123 <marlena@itdex.de>
2025-12-23 09:30:58 -0500

    squeezi squeeze

--- src/eval/patch_predict.py
+++ src/eval/patch_predict.py
@@ -466,6 +466,8 @@ def evaluate_seg(
 
             if logits.dim() == 3:
                 logits = torch.squeeze(logits)
+            elif logits.dim() == 1:
+                logits = torch.unsqueeze(logits, 0)
 
             all_preds_1D.append(
                 rearrange(logits, "b s -> (b s)").float().cpu().numpy()
