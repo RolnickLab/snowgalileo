2e69967a886d2a635a7a22ba2340b797e8aac59d
marlens123 <marlena@itdex.de>
2026-01-17 22:50:29 -0500

    fixes

--- src/eval/hr_eval.py
+++ src/eval/hr_eval.py
@@ -8,7 +8,7 @@ import xarray as xr
 from einops import rearrange
 
 from src.config import DEFAULT_SEED
-from src.data.config import NO_DATA_VALUE
+from src.data.config import NO_DATA_VALUE, CHANNEL_WISE_INVALID_DATA_THRESHOLDS
 from src.data.dataset import Dataset as BaseDataset
 from src.data.earthengine.eo_eval import (
     CLOUD_BANDS,
@@ -34,22 +34,65 @@ class HRMetaDataset(BaseDataset):
             data_folder=data_folder, download=download, h5pys_only=h5pys_only, *args, **kwargs
         )
 
+    @classmethod
+    def _tif_to_array(cls, tif_path: Path):
+        """
+        Loads a spatiotemporal tif file, divides it into different array groups, and creates valid data masks.
+
+        The different array types are:
+        space_time_high_res_x: (H, W, T, C_STH)
+        space_time_med_res_x: (3, 3, T, C_STM)
+        space_time_low_res_x: (2, 2, T, C_STL)
+        space_x: (H, W, C_SP)
+        time_x: (T, C_T)
+        static_x: (C_ST)
+
+        space_time_med_res_x and space_time_low_res_x are created by taking the block mean of their high res version.
+        valid data masks are created by masking out values below a channel-specific threshold (0: invalid, 1: valid).
+        """
+        with cast(xr.Dataset, rioxarray.open_rasterio(tif_path)) as data:
+            # [all_combined_bands, H, W]
+            # all_combined_bands includes all dynamic-in-time bands
+            # interleaved for all timesteps
+            # followed by the static-in-time bands
+            values = cast(np.ndarray, data.values)
+
+        num_timesteps = (values.shape[0] - len(EE_SPACE_BANDS)) / len(EO_ALL_DYNAMIC_IN_TIME_BANDS)
+        assert num_timesteps % 1 == 0, f"{tif_path} has incorrect number of channels"
+        assert num_timesteps == NUM_TIMESTEPS, f"{tif_path} has incorrect number of timesteps"
+        dynamic_in_time_x = rearrange(
+            values[: -(len(EE_SPACE_BANDS))],
+            "(t c) h w -> h w t c",
+            c=len(EO_ALL_DYNAMIC_IN_TIME_BANDS),
+            t=int(num_timesteps),
+        )
+        dynamic_in_time_x = cls._check_and_fillna(
+            dynamic_in_time_x, EO_ALL_DYNAMIC_IN_TIME_BANDS_NP
+        )
+        space_time_high_res_x = dynamic_in_time_x[
+            :,
+            :,
+            :,
+            : -(
+                len(SPACE_TIME_MED_RES_BANDS)
+                + len(EO_SPACE_TIME_LOW_RES_BANDS)
+                + len(TIME_BANDS)
+                + len(CLOUD_BANDS)
+            ),
+        ]
+        valid_mask_s_t_h = space_time_high_res_x != NO_DATA_VALUE
+        for ch, lower_bound in CHANNEL_WISE_INVALID_DATA_THRESHOLDS["s_t_h_x"].items():
+            valid_mask_s_t_h[..., ch] &= space_time_high_res_x[..., ch] >= lower_bound
+
+        try:
+            assert not np.isnan(space_time_high_res_x).any(), f"NaNs in s_t_h_x for {tif_path}"
+            assert not np.isinf(space_time_high_res_x).any(), f"Infs in s_t_h_x for {tif_path}"
+            return space_time_high_res_x.astype(np.half), valid_mask_s_t_h
+        except AssertionError as e:
+            raise e
+
     def _get_hr(self, tif_path: Path):
-        (
-            s_t_h_x,
-            _,
-            _,
-            _,
-            _,
-            _,
-            _,
-            valid_data_mask_s_t_h,
-            _,
-            _,
-            _,
-            _,
-            _,
-        ) = self._tif_to_array(tif_path)
+        s_t_h_x, valid_data_mask_s_t_h = self._tif_to_array(tif_path)
         s_t_h_x = np.where(valid_data_mask_s_t_h, s_t_h_x, NO_DATA_VALUE)
 
         num_hr_days = 0
