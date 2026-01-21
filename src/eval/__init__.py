ba8ca15ce17e496b56fd6eead3786bea173a76a8
marlens123 <marlena@itdex.de>
2026-01-21 17:11:15 -0500

    additions to time ablate

--- src/eval/__init__.py
+++ src/eval/__init__.py
@@ -1,7 +1,9 @@
 from src.eval.landsat_eval import LandsatEval
 from src.eval.sensor_ablations import SensorAblationsEval
+from src.eval.timeseries_ablations import TimeseriesAblationsEval
 
 __all__ = [
     "LandsatEval",
     "SensorAblationsEval",
+    "TimeseriesAblationsEval",
 ]
