e06b96a0880e8a0930d2ba2e2a13f1e77345a721
marlens123 <marlena@itdex.de>
2026-01-26 14:42:55 -0500

    list type

--- src/eval/landsat_eval.py
+++ src/eval/landsat_eval.py
@@ -1623,6 +1623,8 @@ class LandsatEval(EvalTask):
                     encodings = encodings.cpu().numpy()
 
                     for model in sklearn_models:
+                        # TODO: change the type
+                        model = model[0]
                         preds = model.predict(encodings)
                         # reshape the predictions to match the label shape
                         preds_2D = preds.reshape(labels.shape)
