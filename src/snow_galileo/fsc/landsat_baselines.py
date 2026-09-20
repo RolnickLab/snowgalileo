48a665dff183098f5df7e5446c0293164736be9d
marlens123 <marlena@itdex.de>
2026-09-20 10:32:22 +0200

    reshape error

--- src/snow_galileo/fsc/landsat_baselines.py
+++ src/snow_galileo/fsc/landsat_baselines.py
@@ -1129,7 +1129,7 @@ class LandsatEvalSklearn(LandsatEval):
         )
 
         results["baseline"]["patchy_tiles"]["regression"] = compute_regression_metrics(
-            all_preds_2D_f.reshape(-1).numpy(), all_labels_2D_f.reshape(-1).numpy()
+            all_preds_2D_f, all_labels_2D_f
         )
 
         results["model"]["classification"] = compute_classification_metrics(
